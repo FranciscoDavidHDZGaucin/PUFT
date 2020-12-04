a6f06b56ba73426cc51d70a1a9c261416fe20764
fhernandez <fhernandez@VDI-TIC03>
2020-12-04 10:06:10 -0600

    ESTRUCTURA PARA MANDAR A INSERTAR PRODUCTOS PRUEBA 001

---
+++ PUFT_PRUEBA_001/class/ACCION_PRODUCTOS_PEDIDOS.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PUFT_PRUEBA_001{
+    class ACCION_PRODUCTOS_PEDIDOS
+
+
+    {
+        public CTRL_OBJET _CONTROL_ORDEVENTA;
+        private DataTable tb_productos;
+
+        public ACCION_PRODUCTOS_PEDIDOS(CTRL_OBJET   _CTRLORDVENTA     )
+        {
+
+            try
+             {
+                this._CONTROL_ORDEVENTA = _CTRLORDVENTA;
+                //VALIDAMOS QUE ESTE AUTORIZADO  LA  ORDEN DE VENTA 
+                if (this._CONTROL_ORDEVENTA.CONTROL_MAIN)
+                {
+
+                  
+                    tb_productos = new DataTable();
+                    try
+                    {
+                        var connection =
+                                      System.Configuration.ConfigurationManager.
+                                      ConnectionStrings["SALESFOREC"].ConnectionString;
+                        using (SqlConnection CONECT = new SqlConnection(connection))
+                        {
+                            CONECT.Open();
+                            using (SqlCommand COMANDO = new SqlCommand("SP_PUFT_PRODUCTOS_PEDIDOS", CONECT))
+
+
+                            {
+                                COMANDO.CommandType = CommandType.StoredProcedure;
+                                COMANDO.Parameters.Add("@ORDEN_VENTA", SqlDbType.BigInt).Value = _CONTROL_ORDEVENTA.ORD_VENTA;
+                                tb_productos.Load(COMANDO.ExecuteReader());
+
+                                _CONTROL_ORDEVENTA.TB_PRODUCTOS = tb_productos; 
+
+
+                            }
+                        }
+
+                    }
+                    catch (Exception e)
+                    {
+                        tb_productos = new DataTable();
+                    }
+
+
+                }
+
+
+            }
+            catch (Exception e)
+            { }
+
+
+
+
+        }
+
+        public  void  RECORRER_PRODUCTOS()
+       {
+
+            if (_CONTROL_ORDEVENTA.CONTROL_MAIN)
+            {
+                if (tb_productos.Rows.Count > 0)
+                {
+                    foreach (DataRow row in tb_productos.Rows)
+                    {
+                        var  prueba =   row["ORDENDE_VENTA"].ToString(); 
+                        //// METER KLA CLASE DE INSERTSIO DE   PRODUCTOS  
+
+                    }
+
+
+
+
+                }
+
+            }
+
+        }
+
+
+    }
+
+
+
+}
---
+++ PUFT_PRUEBA_001/class/CTRL_OBJET.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PUFT_PRUEBA_001
+{
+    class CTRL_OBJET
+    {
+        private Boolean _CONTROL_MAIN = false ; 
+        private  Int64 _REMISION;
+        private Boolean _Esnew;
+        private Boolean _ExisteORDVENTA;
+        private int _ORD_VENTA;
+        private DataTable tb_productos;
+
+        public CTRL_OBJET(int ORD_VENTA, Boolean ExisteORDVENTA, Int64 REMISION, Boolean Esnew)
+        {
+            try
+            {
+                this._REMISION = REMISION;
+                this._Esnew = Esnew;
+                this._ExisteORDVENTA = ExisteORDVENTA;
+                this._ORD_VENTA = ORD_VENTA;
+                if (this._ExisteORDVENTA && this._Esnew)
+                {
+                    this._CONTROL_MAIN = true;
+
+                }
+            }
+            catch (Exception e)
+            {
+
+
+            }
+
+
+        }
+
+        public  Int64 REMISION { get  => _REMISION ;  }
+        public Boolean EsNew { get => _Esnew;  }
+        public Boolean ExisteORDVENTA { get => _ExisteORDVENTA;  }
+        public int ORD_VENTA { get => _ORD_VENTA; }
+        public DataTable TB_PRODUCTOS { get => tb_productos; set => tb_productos = value; }
+
+        public Boolean CONTROL_MAIN { get => _CONTROL_MAIN;  } 
+    }
+}
