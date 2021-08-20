6a51a873f12e98f65c0463c9e3675023efe85e64
fhernandez <fhernandez@VDI-TIC03>
2021-08-20 12:28:29 -0500

    Agregado  de Ado .net para Agroversa Productiva Agregamos   funcion de insertar al   80 faltan campos generamos Clase CTRL_DIRECCIONES   con estructura elemental

---
+++ PUFT_PRUEBA_001/class/Cls_Dirreciones/CTRL_DIRECCIONES.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PUFT_PRUEBA_001
+{
+    
+    class CTRL_DIRECCIONES
+{
+
+        public CTRL_DIRECCIONES()
+        {
+            using (AGROVERSA_PRODUCTIVAEntities Dbo = new AGROVERSA_PRODUCTIVAEntities())
+            {
+               
+                foreach(VW_PUFT_VALI_INSER_DIREC row in Dbo.VW_PUFT_VALI_INSER_DIREC)
+                {
+                    if (row.APLI_INSERT == 1)
+                    {
+
+
+                    }
+
+
+                }
+
+
+
+            }
+
+        }
+          
+
+
+}
+}
--- PUFT_PRUEBA_001/class/cmdsForm.cs
+++ PUFT_PRUEBA_001/class/cmdsForm.cs
@@ -390,6 +390,40 @@ namespace PUFT_PRUEBA_001
             return exito;
         }
 
+        public bool insertDirrecciones(VW_PUFT_VALI_INSER_DIREC  ObjDir )
+        {
+            bool exito = false;
+            try
+            {
+                MySqlCommand command = new MySqlCommand("SP_PUFT_INSERT_DIRECCIONES", conexion);
+                command.Connection = conexion;
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("InsClvecte", ObjDir.CardCode);
+                command.Parameters.AddWithValue("InsLineNum", ObjDir.LineNum);
+                command.Parameters.AddWithValue("U_IDSFDC", ObjDir.UID_SFDC);
+                command.Parameters.AddWithValue("Insnom_cliente", ObjDir.);
+                command.Parameters.AddWithValue("Inscalle", ObjDir.);
+                command.Parameters.AddWithValue("Inscolonia", ObjDir.);
+                command.Parameters.AddWithValue("Insciudad", ObjDir.);
+                command.Parameters.AddWithValue("Inscp", ObjDir.);
+                command.Parameters.AddWithValue("Insestado", ObjDir.);
+                command.Parameters.AddWithValue("Inspais", ObjDir.);
+                command.Parameters.AddWithValue("Inszona", ObjDir.);
+                command.Parameters.AddWithValue("InsAdresType", ObjDir.);
+              
+            }
+            catch (Exception e)
+            {
+                exito = false;
+                DateTime thisDay = DateTime.Today;
+                // Display the date in the default (general) format.
+                PUFT_ERRORS error = new PUFT_ERRORS("Class cmdsForm/insertDirrecciones", "ERROR INSERTAR DIRRECION SAP=>MYSQL", e.ToString(), thisDay);
+
+            }
+            return exito; 
+        }
+
+
         public bool insertarDatosFactura(double id_detalle, Int64 n_remision, Int64 n_agente, string nom_age, DateTime fecha_alta, string cve_cte, string nom_cte, string cve_prod,
                  string nom_prod, double cant_prod, double cant_original, double precio_prod, double dcto_prod, double ieps, double iva, double total_prod,
                  Int64 moneda_prod, double cant_falta, double tipo_cambio, double precio_condcto, double precio_politica, string bonificacion,
